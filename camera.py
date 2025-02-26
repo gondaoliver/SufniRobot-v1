9590ab292ded523aeb218d086641624559aa1cfd
gondaoliver <gondaolivander@gmail.com>
2025-02-26 14:07:37 +0100

    Inital commit

--- camera.py
+++ camera.py
@@ -1,10 +1,13 @@
 import cv2
 
-cap = cv2.VideoCapture(0) 
+cap = cv2.VideoCapture(0)
+
+# Set the frame width and height
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 while True:
     ret, frame = cap.read() 
-
 
     cv2.imshow('SufniCam', frame)  
 
